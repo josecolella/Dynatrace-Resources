6b73ef1e42877c4000531fdc90b57e3eb490768b
josecolella <josecolella@yahoo.com>
2015-09-03 12:36:04 +0200

    Can fetch xf consumption

--- Dynatrace_SaaS/Portal/Portal.py
+++ Dynatrace_SaaS/Portal/Portal.py
@@ -3,12 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from abc import ABCMeta
 from abc import abstractmethod
 import logging
 import time
-import numpy as np
 import re
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class XFConsumption(object):
@@ -18,8 +21,13 @@ class XFConsumption(object):
     in the GPN portal"""
 
     def __init__(self):
+        self.lettersRegex = re.compile(r'[a-zA-Z]')
         self.usage = None
         self.endOfMonthProjection = None
+        self.monthlyOffset = None
+        self.tableRows = None
+        self.xfTable = None
+        self.xfSumConsumption = None
 
     def _sanitizeIntegerString(self, intString):
         """
@@ -32,18 +40,64 @@ class XFConsumption(object):
         return sanitizedInteger
 
     def _sanitizeXFTable(self):
-        # TODO
-        lettersRegex = re.compile(r'[a-zA-Z]')
-        print(self.tableRows)
-        # for element, index in zip(self.tableRows, range(self.tableRows.size)):
-            # if not re.search(lettersRegex, element):
-                # self.tableRows[index] = self._sanitizeIntegerString(element)
+        assert self.tableRows is not None, print("tableRows can not be None")
+        for element, index in zip(self.tableRows, range(self.tableRows.size)):
+            if not re.search(self.lettersRegex, element):
+                self.tableRows[index] = self._sanitizeIntegerString(element)
 
-    def setTableRows(self, tableRowsHTML):
-        # TODO
-        self.tableRows = np.array([elem.text for elem in tableRowsHTML])
-        self._sanitizeXFTable()
-        # self.tableRows.resize(self.TableRows.size / 3, self.TableRows.size / 3)
+    def setEndOfMonthProjection(self, projection):
+        assert type(projection) == str, print(
+            "End of Month Projection is a string")
+        self.endOfMonthProjection = self._sanitizeIntegerString(projection)
+
+    def _getNumberOfColumns(self, tableRowsList):
+        assert type(tableRowsList) == list, print(
+            "tableRows expected to be list, but has type: {}".format(type(tableRowsList)))
+        columnIndices = tuple(index for elem, index in zip(
+            tableRowsList, range(len(tableRowsList))) if re.search(self.lettersRegex, elem))
+        return columnIndices[1]
+
+    def setXFTable(self, tableRowsHTML):
+        tableRowsTextList = [elem.text for elem in tableRowsHTML]
+        numColumn = self._getNumberOfColumns(tableRowsTextList)
+        self.xfTable = [
+            {
+                'day': tableRowsTextList[index],
+                'value': max(map(self._sanitizeIntegerString, tableRowsTextList[index + 1:index + numColumn]))
+            }
+            for index in range(0, len(tableRowsTextList), numColumn)]
+
+    def setSumXFConsumption(self, startDay=1, endDay=31):
+        """
+        setSumXFConsumption(startDay, endDay) -> sets the sum of the xfconsumption measures
+
+        Args
+        ----
+            startDay: int
+                - The startday for calculating the xf consumption measures
+            endDay: int
+                - The endday for calculating the xf consumption measures
+
+        """
+        dayRegex = re.compile(r'^\d+ ')
+        xfConsumptionValues = [elem["value"] for elem in self.xfTable if re.match(dayRegex, elem["day"]) and int(
+            re.match(dayRegex, elem["day"]).group()) in range(startDay, endDay + 1)]
+        logging.info(xfConsumptionValues)
+        self.xfSumConsumption = sum(xfConsumptionValues)
+
+    def setMonthlyOffset(self):
+        """
+        setMonthlyOffset() -> sets the monthly offset
+        """
+        assert self.xfTable is not None, print("xfTable can not be None")
+        # Can be optimized using dates
+        self.monthlyOffset = self.xfTable[-
+                                          1]["value"] - self.xfTable[-2]["value"]
+
+    def __str__(self):
+        xfConsumptionString = "End of Month Projection: {}\nMonthly Offset: {}\nSum XF Consumption: {}\n".format(
+            self.endOfMonthProjection, self.monthlyOffset, self.xfSumConsumption)
+        return xfConsumptionString
 
 
 class AbstractPortal(object):
@@ -77,7 +131,6 @@ class AbstractPortal(object):
 class GPNPortal(AbstractPortal):
 
     homePage = "https://www.gomeznetworks.com/index.asp?g=1"
-    xfConsumptionPage = "https://www.gomeznetworks.com/reports/flexReport.aspx?x=&startdate=2015/9/1&enddate=2015/9/30"
     usernameInputName = "username"
     passwordInputName = "pwd"
     submitButtonClass = "Login-Button"
@@ -110,26 +163,26 @@ class GPNPortal(AbstractPortal):
         time.sleep(5)
         self.driver.save_screenshot("gpn2.png")
 
-    def getXFConsumption(self, startDate, endDate):
-        # Open a new tab
-        body = driver.find_element_by_tag_name("body")
-        body.send_keys(Keys.CONTROL + 't')
-        self.driver.get(GPNPortal.xfConsumptionPage)
+    def getXFConsumption(self, startDate=0, endDate=0):
+        # Open a new tab TODO
+        body = self.driver.find_element_by_tag_name("body")
+        body.send_keys(Keys.COMMAND + 't')
+        xfConsumptionPage = "https://www.gomeznetworks.com/reports/flexReport.aspx?x=&startdate=2015/8/1&enddate=2015/8/30"
+        self.driver.get(xfConsumptionPage)
         try:
             WebDriverWait(self.driver, 30).until(
-                EC.visibility_of_element_located((By.ID, endOfMonthProjectionIdentifier)))
+                EC.visibility_of_element_located((By.ID, "ctl00$Content$Chart")))
         except Exception:
             logging.warning("The page could not load")
         xfConsumption = XFConsumption()
-        xfConsumption.endOfMonthProjection = self.driver.find_element_by_id(
-            GPNPortal.endOfMonthProjectionIdentifier).text
-        print("End of the month Projection: {}".format(
-            xfConsumption.endOfMonthProjection))
-        summaryTable = self.driver.find_element_by_id(
-            GPNPortal.tableId)
+        xfConsumption.setEndOfMonthProjection(
+            self.driver.find_element_by_id(GPNPortal.endOfMonthProjectionIdentifier).text)
+        summaryTable = self.driver.find_element_by_id(GPNPortal.tableId)
         xfMeasurementHtmlTable = summaryTable.find_elements_by_tag_name("td")
-        xfConsumption.setTableRows(xfMeasurementHtmlTable)
-        print("Table Rows: {}".format(xfConsumption.tableRows))
+        xfConsumption.setXFTable(xfMeasurementHtmlTable)
+        xfconsumption.setMonthlyOffset()
+        xfconsumption.setSumXFConsumption(1, 31)
+        return xfConsumption
 
     def setAccounts(self):
         # Button needs to be clicked in order to see other accounts
