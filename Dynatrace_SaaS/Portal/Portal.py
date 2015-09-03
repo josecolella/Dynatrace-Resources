50bbe4fc7ceb56fa75fadd005b85c1471ed19d6b
josecolella <josecolella@yahoo.com>
2015-09-03 13:19:13 +0200

    Move DynatracePortal to Portal file. Able to obtain screeshot

--- Dynatrace_SaaS/Portal/Portal.py
+++ Dynatrace_SaaS/Portal/Portal.py
@@ -193,3 +193,94 @@ class GPNPortal(AbstractPortal):
         accountListRows = accountList.find_elements_by_tag_name("tr")[1:-1]
         self.accounts = [{'name': accountListRow.text, 'node': accountListRow}
                          for accountListRow in accountListRows]
+
+
+class DynatracePortal(AbstractPortal):
+
+    homePage = "https://www.gomezapm.com"
+    usernameInputId = "username"
+    passwordInputId = "password"
+    submitButtonId = "signIn"
+    monitorAnalyzeId = "monitoranalyze"
+    interactiveChartId = "apmInteractiveChart"
+    chartsClass = "apm-btn-link"
+    logoutId = "sign-out"
+    iframeName = "apmframe"
+
+    def login(self):
+        """
+        login()
+        """
+        logging.debug("Fetching Dynatrace Login Page")
+        self.driver.get(DynatracePortal.homePage)
+        logging.debug("Finish fetching page")
+        usernameInput = self.driver.find_element_by_id(
+            DynatracePortal.usernameInputId)
+        passwordInput = self.driver.find_element_by_id(
+            DynatracePortal.passwordInputId)
+        logging.debug("Sending username credentials")
+        usernameInput.send_keys(self.username)
+        logging.debug("Sending password credentials")
+        passwordInput.send_keys(self.password)
+        submitButton = self.driver.find_element_by_id(
+            DynatracePortal.submitButtonId)
+        logging.debug("Sending button click")
+        submitButton.click()
+        logging.debug("Waiting for page to load")
+        try:
+            WebDriverWait(self.driver, 5).until(
+                EC.presence_of_element_located((By.ID, DynatracePortal.monitorAnalyzeId)))
+        except Exception:
+            logging.warning("The page could not load")
+
+    def getCharts(self,index = 0, upper = 3):
+        """
+        """
+        logging.debug("Clicking Monitor & Analyze Tab")
+        self.driver.find_element_by_id(
+            DynatracePortal.monitorAnalyzeId).click()
+        self.driver.save_screenshot('here{}.png'.format(index))
+        logging.debug("Getting chart link element")
+        chartLink = self.driver.find_element_by_id(
+            DynatracePortal.interactiveChartId)
+        href = chartLink.get_attribute("href")
+        logging.debug(href)
+        self.driver.get(href)
+        # try:
+        self.driver.save_screenshot('here2{}.png'.format(index))
+        try:
+            WebDriverWait(self.driver, 30).until(
+                EC.visibility_of_element_located((By.ID, "apmframe")))
+            # Switch to iframe in order to obtain DOM elements
+            self.driver.switch_to_frame(DynatracePortal.iframeName)
+        except Exception:
+            logging.warn("Element could not be found within the time frame")
+        logging.debug("Sleeping for 5 seconds")
+        time.sleep(5)
+        self.driver.save_screenshot('here3{}.png'.format(index))
+        test = self.driver.find_element_by_id("wrapper").get_attribute("class")
+        logging.debug("Class is: {}".format(test))
+        test2 = self.driver.find_element_by_class_name(
+            "dataTable").get_attribute("style")
+        logging.debug("Style is: {}".format(test2))
+        test3 = self.driver.find_elements_by_class_name(
+            DynatracePortal.chartsClass)
+        logging.debug("Charts are: {}".format(test3))
+        test3classes = list(filter(lambda node: DynatracePortal.chartsClass in node.get_attribute(
+            "class") and node.text != '', test3))
+        print("Classes are: {}".format([elem.text for elem in test3classes]))
+        self.driver.save_screenshot('here4{}.png'.format(index))
+        print(test3classes[0])
+        print("Clicking on element")
+        print(test3classes[0].click())
+        # logging.debug("Saving screenshot")
+        time.sleep(25)
+        self.driver.save_screenshot('here5{}.png'.format(index))
+
+
+
+    def logout(self):
+        pass
+        # signOutButton = self.driver.find_element_by_id(
+        #     DynatracePortal.logoutId)
+        # signOutButton.click()
