7e464f70157c2ce7382912d65014bf9d872bed75
josecolella <josecolella@yahoo.com>
2015-09-19 15:29:00 +0200

    Add command line arguments for program

--- portal/main.py
+++ portal/main.py
@@ -1,24 +1,28 @@
 import argparse
 import logging
+import Portal
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog="BTScreenshotAutomation")
+    parser = argparse.ArgumentParser(prog="Dynatrace Synthetic Automation")
     parser.add_argument(
-        "-a", "--account", help="The name of the account", type=str, required=True)
+        "-t", "--type", help="The account type: [gpn|dynatrace]", required=True)
     parser.add_argument(
-        "-c", "--additionalCharts", nargs="+", help="The name of the additional charts")
+        "-u", "--username", help="The username for the account", type=str, required=True)
+    parser.add_argument(
+        "-p", "--password", help="The password for the account", type=str, required=True)
     parser.add_argument(
         "-d", "--directory", help="The directory to save the chart screenshots", type=str, default=".")
     parser.add_argument(
         "-v", "--verbose", help="Display debug message", action="store_true")
+    parser.add_argument(
+        "-c", "--chartNames", nargs="+", help="The name of the chart to capture")
     args = parser.parse_args()
-    additionalCharts = set()
-    if args.additionalCharts is not None:
-        additionalCharts = set(args.additionalCharts)
-    btSeedingAccount = BTSeeding.BTSeedingAccount(
-        args.account, additionalCharts)
-    btSeedingAccount.run(saveDir=args.directory)
+    print(args)
+    # portal = Portal.DynatracePortal(args.username, args.password)
+    # portal.login()
+    # portal.saveChartToScreenshot()
